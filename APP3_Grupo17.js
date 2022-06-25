cec306bc780e3919a3aabe050b5e6ae1bb3f88f8
josecastilloUai <95188754+josecastilloUai@users.noreply.github.com>
2022-06-24 22:57:55 -0400

    Update APP3_Grupo17.js

--- APP3_Grupo17.js
+++ APP3_Grupo17.js
@@ -1,4 +1,8 @@
 var _= require("underscore");
 const {readFileSync, promises: fsPromises} = require('fs');
 
-
+function syncReadFile(filename) { //Función que lee el archivo.
+  const contents = readFileSync(filename, 'utf-8');
+  const arr = contents.split(/\r?\n/); //Separa el archivo texto por fila.
+  return arr;
+}
