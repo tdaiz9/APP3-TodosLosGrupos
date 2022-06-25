ccb96a2b707d19745580c64684a20ee055afebfe
tdaiz9 <101015433+tdaiz9@users.noreply.github.com>
2022-06-24 23:00:37 -0400

    Update APP3_Grupo17.js

--- APP3_Grupo17.js
+++ APP3_Grupo17.js
@@ -6,6 +6,7 @@ function syncReadFile(filename) { //Función que lee el archivo.
   const arr = contents.split(/\r?\n/); //Separa el archivo texto por fila.
   return arr;
 }
+
 function toNumber(array,i){ //Función que convierte una matriz (matriz de matrices en este caso) de strings en una de números.
   if(array[i]==null){} //Caso base. Si es nulo, se termina la función y retorna null.
   else {
@@ -13,4 +14,13 @@ function toNumber(array,i){ //Función que convierte una matriz (matriz de matri
     toNumber(array,i+1); //Se hace lo mismo con la siguiente fila.
   }
   return array; //Retorna el array.
+}
+
+function splitrow(data,i) {//Función separa las filas. 
+  if (data[i]==null){return}//Caso base. Si es nulo, se termina la función y retorna null.
+  else{
+    data[i]=data[i].split(" "); //Se separa las filas por espacio.
+    splitrow(data,i+1); //Se hace lo mismo con la siguiente fila.
+  }
+  return toNumber(data,0); //Se retorna la matriz (se pasa de strings a números).
 }
